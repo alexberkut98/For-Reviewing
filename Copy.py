ce2f41c26a0d3959be55cc58b89ad1ec9bd0ff6d
Roman <121679425+RomRomich@users.noreply.github.com>
2023-04-15 12:05:43 +0300

    2222
    22222222

--- Copy.py
+++ Copy.py
@@ -1,6 +1,6 @@
 # time the lasagna should be in the oven according to the cookbook.
-EXPECTED_BAKE_TIME = 45454534
-PREPARATION_TIME = 54543534
+EXPECTED_BAKE_TIME = 4545453411111
+PREPARATION_TIME = 54543534111111111
 def bake_time_remaining(elapsed_bake_time):
     """Calculate the bake time remaining.
  
